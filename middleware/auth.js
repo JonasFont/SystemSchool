27f4ae9905e2783192fc3ab09ce5c25a6b5ad7b7
JonasFont <89553471+JonasFont@users.noreply.github.com>
2025-10-08 20:44:39 -0300

    Update auth.js
    Script de auth de rotas

---
+++ middleware/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+function auth(req, res, next){
+    const token = req.header('x-auth-token');
+    if (!token) return res.status(401).json({msg: 'Sem token, acesso negado'});
+
+    try{
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    }catch (err){
+        res.status(400).json({ msg: 'Token inválido'})
+    }
+}
+module.exports =auth;
